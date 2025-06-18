b9df655ba6152e1db96e50deb0b98296c93540b4
Mirelle Alves <mirellealves@outlook.com>
2025-06-18 18:41:55 -0300

    atualização model

--- hohost-app/hohost-app/src/main/java/br/edu/cads/hohostapp/model/Anfitriao.java
+++ hohost-app/hohost-app/src/main/java/br/edu/cads/hohostapp/model/Anfitriao.java
@@ -1,5 +1,21 @@
 package br.edu.cads.hohostapp.model;
 
+import java.util.List;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
 public class Anfitriao {
-    
+    @Id 
+    @GeneratedValue(strategy = GenerationType.IDENTITY) private Long id;
+    private String nome;
+    private String telefone;
+    private String email;
+    private String estado;
+    private String cidadeAtuação;
+
+    @OneToMany(mappedBy = "anfitriao")
+    private List<Acomodacao> acomodacoes;
 }
